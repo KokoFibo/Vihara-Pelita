7940444ce922581788db541696c7b8ee1092747b
Anton <kokonacci@gmail.com>
2022-11-27 19:12:05 +0700

    width full

--- resources/js/Pages/data/Index.vue
+++ resources/js/Pages/data/Index.vue
@@ -187,7 +187,7 @@ function check_JK(jk, umur) {
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="w-full mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <!-- Tab -->
                     <div class="tabs mb-5">
